4a88fbc053e67de55f50782c884006cbcd9acd7a
Ben <ben@armosec.io>
2022-12-06 21:58:45 +0200

    cetificate generation

---
+++ generate-certificate-for-ip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Generate a self-signed certificate for a given IP address
+# Usage: generate-certificate-for-ip.sh <IP address>
+# Example: generate-certificate-for-ip.sh 10.0.0.1
+IP=$1
+if [ -z "$IP" ]; then
+    echo "Usage: generate-certificate-for-ip.sh <IP address>"
+    exit 1
+fi
+
+pushd certs || exit 1
+
+# Replace dots to dashes in string variable
+IP_DASHED=`echo $IP | sed -e "s/\./-/g"`
+
+# Replace the IP address in the configuration file
+sed -e "s/IPADDRESS/$IP/" cert.conf.tmpl > cert-$IP_DASHED.conf
+
+# Generate certificate signing request
+openssl req -new -key proxy-server-clear.key -out proxy-server-$IP_DASHED.csr -config cert-$IP_DASHED.conf || exit 1
+
+# Empty ca.db.index file
+truncate -s 0 ca.db.index
+
+# Sign the certificate signing request
+openssl ca -batch -config ca.conf -in proxy-server-$IP_DASHED.csr -out proxy-server-$IP_DASHED.crt 
+
+popd
